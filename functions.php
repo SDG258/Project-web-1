224b7c5b88842b84b7eb21c64eedf00159876ff8
Du Nguyễn Song <1760033@student.hcmus.edu.vn>
2019-12-27 18:17:59 +0700

    message with no realtime

--- functions.php
+++ functions.php
@@ -1,38 +1,43 @@
 <?php
 
-require_once ('./vendor/autoload.php');
+require_once('./vendor/autoload.php');
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-function findUserByEmail ($email) {
+function findUserByEmail($email)
+{
     global $db;
-    $stmt =$db->prepare("select * from user where email = ?");
+    $stmt = $db->prepare("select * from user where email = ?");
     $stmt->execute(array($email));
     return $stmt->fetch(PDO::FETCH_ASSOC);
 }
-function findUserById ($id) {
+function findUserById($id)
+{
     global $db;
     $stmt = $db->prepare("select * from user where id = ?");
     $stmt->execute(array($id));
     return $stmt->fetch(PDO::FETCH_ASSOC);
 }
-function detetPage(){
+function detetPage()
+{
     $uri = $_SERVER['REQUEST_URI'];
     $parts = explode('/', $uri);
-    $fileName = $parts[count($parts)-1];
+    $fileName = $parts[count($parts) - 1];
     $parts = explode('.', $fileName);
     $page = $parts[0];
     return $page;
 }
-function updateUserPassword($id, $password){
+function updateUserPassword($id, $password)
+{
     global $db;
     $hashPassword = password_hash($password, PASSWORD_DEFAULT);
     $stmt = $db->prepare("UPDATE user SET password = ? Where id = ?");
     return $stmt->execute(array($hashPassword, $id));
 }
-function createUser($firstName, $surname, $displayName, $gender, $email, $password, $DOB, $phoneNumber){
+function createUser($firstName, $surname, $displayName, $gender, $email, $password, $DOB, $phoneNumber)
+{
     global $db, $BASE_URL;
     $hashPassword = password_hash($password, PASSWORD_DEFAULT);
     $code = generateRandomString(16);
@@ -42,29 +47,34 @@ function createUser($firstName, $surname, $displayName, $gender, $email, $passwo
     sendEmail($email, $displayName, 'Kích hoạt tài khoản', "Để kích hoạt tài khoản vui lòng ấn vào đường link: <a href = \"$BASE_URL/activate.php?code=$code&&email=$email\">$BASE_URL/activate.php?code=$code&&email=$email</a>");
     return $id;
 }
-function updateProfile($firstName, $surname, $displayName, $DOB, $phoneNumber, $mime, $avatars, $id){
+function updateProfile($firstName, $surname, $displayName, $DOB, $phoneNumber, $mime, $avatars, $id)
+{
     global $db;
     $stmt = $db->prepare("UPDATE `user` SET `firstName`= ?, `surname` = ?, `displayName` = ?, `DOB` = ?, `phoneNumber` = ?, `mime` = ?, `avatars` =  ? WHERE `user`.`id` = ?");
     return $stmt->execute(array($firstName, $surname, $displayName, $DOB, $phoneNumber, $mime, $avatars, $id));
 }
-function getNewFeeds(){
+function getNewFeeds()
+{
     global $db;
     $stmt = $db->query("SELECT p.*, u.displayName, u.id as idAvatar FROM posts AS p JOIN user AS u ON p.userID = u.id ORDER BY createdAt DESC");
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
-function getMyStatus($userID){
+function getMyStatus($userID)
+{
     global $db;
     $stmt = $db->prepare("SELECT p.*, u.displayName, u.id as idAvatar, u.DOB  FROM posts AS p JOIN user AS u ON p.userID = u.id Where u.id = ? ORDER BY createdAt DESC");
     $stmt->execute(array($userID));
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
-function createPost($userID, $content,$mime,$image,$privacy){
+function createPost($userID, $content, $mime, $image, $privacy)
+{
     global $db;
     $stmt = $db->prepare("INSERT INTO posts (content, userID,mime,image,privacy) VALUES (? ,? ,? ,? ,?)");
     $stmt->execute(array($content, $userID, $mime, $image, $privacy));
 }
-function generateRandomString($length = 10) {
+function generateRandomString($length = 10)
+{
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
     $randomString = '';
@@ -73,7 +83,8 @@ function generateRandomString($length = 10) {
     }
     return $randomString;
 }
-function sendEmail($to, $name, $subject, $content){
+function sendEmail($to, $name, $subject, $content)
+{
 
     global $EMAIL_FROM, $EMAIL_PASSWORD, $EMAIL_NAME;
 
@@ -82,16 +93,16 @@ function sendEmail($to, $name, $subject, $content){
     //Server settings                
     $mail->isSMTP();
     $mail->CharSet    = 'UTF-8';
-    $mail->Host       = 'smtp.gmail.com';                    
-    $mail->SMTPAuth   = true;                                   
-    $mail->Username   = $EMAIL_FROM;                   
+    $mail->Host       = 'smtp.gmail.com';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = $EMAIL_FROM;
     $mail->Password   = $EMAIL_PASSWORD;
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port       = 587;
 
     //Recipients
     $mail->setFrom($EMAIL_FROM, $EMAIL_NAME);
-    $mail->addAddress($to, $name); 
+    $mail->addAddress($to, $name);
 
     // Content
     $mail->isHTML(true);
@@ -101,86 +112,91 @@ function sendEmail($to, $name, $subject, $content){
 
     $mail->send();
 }
-function activateUser($code){
+function activateUser($code)
+{
     global $db;
-    $stmt =$db->prepare("select * from user where code = ? and status = ?");
+    $stmt = $db->prepare("select * from user where code = ? and status = ?");
     $stmt->execute(array($code, '0'));
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
-    if ($user && $user['code'] == $code){
+    if ($user && $user['code'] == $code) {
         $stmt = $db->prepare("UPDATE user SET code = ?, status = ? Where id = ?");
         $stmt->execute(array('', '1', $user['id']));
         return true;
     }
     return false;
 }
-function crateCodeForgetPassword($code, $email){
+function crateCodeForgetPassword($code, $email)
+{
     global $db;
     $stmt = $db->prepare("UPDATE `user` SET `code` = ? WHERE`user`.`email` = ?");
     return $stmt->execute(array($code, $email));
 }
-function activatePassword($code){
+function activatePassword($code)
+{
     global $db;
-    $stmt =$db->prepare("select * from user where code = ?");
+    $stmt = $db->prepare("select * from user where code = ?");
     $stmt->execute(array($code));
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
-    if ($user && $user['code'] == $code){
+    if ($user && $user['code'] == $code) {
         $stmt = $db->prepare("UPDATE user SET code = ? Where id = ?");
         $stmt->execute(array('', $user['id']));
         return true;
     }
     return false;
 }
-function loadAvatars($id){
+function loadAvatars($id)
+{
     global $db;
-    $stmt =$db->prepare("select * from user where id = ?");
+    $stmt = $db->prepare("select * from user where id = ?");
     $stmt->execute(array($id));
     return $stmt->fetch(PDO::FETCH_ASSOC);
 }
-function loadImage($id){
+function loadImage($id)
+{
     global $db;
-    $stmt =$db->prepare("select * from posts where id = ?");
+    $stmt = $db->prepare("select * from posts where id = ?");
     $stmt->execute(array($id));
     return $stmt->fetch(PDO::FETCH_ASSOC);
 }
 
-function test_input($data) {
-	$data = trim($data);
-	$data = stripslashes($data);
-	$data = htmlspecialchars($data);
-	return $data;
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
 }
 function sendFriendRequest($userId1, $userId2)
 {
-  global $db;
-  $stmt = $db->prepare("INSERT INTO friendship (userId1,userId2) VALUES (?, ?) ");
-  $stmt->execute(array($userId1, $userId2));
+    global $db;
+    $stmt = $db->prepare("INSERT INTO friendship (userId1,userId2) VALUES (?, ?) ");
+    $stmt->execute(array($userId1, $userId2));
 
-  sendNotificationAddFriend($userId1, $userId2);
-  
+    sendNotificationAddFriend($userId1, $userId2);
 }
 
 function removeFriendRequest($userId1, $userId2)
 {
-  global $db;
-  $stmt = $db->prepare("DELETE FROM friendship WHERE (userId1 = ? AND userId2=?) OR (userId2 = ? AND userId1=?) ");
-  $stmt->execute(array($userId1, $userId2,$userId1, $userId2));
+    global $db;
+    $stmt = $db->prepare("DELETE FROM friendship WHERE (userId1 = ? AND userId2=?) OR (userId2 = ? AND userId1=?) ");
+    $stmt->execute(array($userId1, $userId2, $userId1, $userId2));
 }
 
-function getFriendShip($userId1,$userId2)
+function getFriendShip($userId1, $userId2)
 {
-  global $db;
-  $stmt = $db->prepare("SELECT * FROM friendship WHERE userId1 = ? AND userId2=?");
-  $stmt->execute(array($userId1, $userId2));
-  return $stmt->fetch(PDO::FETCH_ASSOC);
+    global $db;
+    $stmt = $db->prepare("SELECT * FROM friendship WHERE userId1 = ? AND userId2=?");
+    $stmt->execute(array($userId1, $userId2));
+    return $stmt->fetch(PDO::FETCH_ASSOC);
 }
 
 
 
-function addCommentToPost($postId,$userId,$content)
+function addCommentToPost($postId, $userId, $content)
 {
     global $db;
     $stmt = $db->prepare("INSERT INTO comments (postId,userId,content) VALUES (?, ?, ?) ");
-    $stmt->execute(array($postId,$userId,$content));
+    $stmt->execute(array($postId, $userId, $content));
 }
 
 function getCommentOfPost($postId)
@@ -231,104 +247,136 @@ function loadPostOfUserForEveryOne($id)
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
-function userLike($postId,$userId)
+function userLike($postId, $userId)
 {
-    
+
     global $db;
     $stmt = $db->prepare("SELECT *FROM likes WHERE postId=? and userId=?");
-    $stmt->execute(array($postId,$userId));
-    if($stmt->fetchAll(PDO::FETCH_ASSOC)) 
-    {
+    $stmt->execute(array($postId, $userId));
+    if ($stmt->fetchAll(PDO::FETCH_ASSOC)) {
         return true;
     }
     return false;
 }
-function addOrRemoveLike($postId,$userId)
+function addOrRemoveLike($postId, $userId)
 {
     global $db;
-    if(userLike($postId,$userId))
-    {
+    if (userLike($postId, $userId)) {
         $stmt = $db->prepare("DELETE FROM likes WHERE postId=? and userId=?");
-        $stmt->execute(array($postId,$userId));
+        $stmt->execute(array($postId, $userId));
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-    else{
+    } else {
         $stmt = $db->prepare("INSERT INTO likes (postId,userId) VALUES (?, ?)");
-        $stmt->execute(array($postId,$userId));
+        $stmt->execute(array($postId, $userId));
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
-function sendNotificationAddFriend($fromUserId,$toUserId)
+function sendNotificationAddFriend($fromUserId, $toUserId)
 {
     global $BASE_URL;
-    $user=findUserById($toUserId);
-    $currentUser=findUserById($fromUserId);
+    $user = findUserById($toUserId);
+    $currentUser = findUserById($fromUserId);
 
-    sendEmail($user['email'],$user['displayName'],'Lời mời kết bạn', $currentUser['displayName'] ." đã gửi cho bạn một lời mời kết bạn.Link đến trang cá nhân: 
+    sendEmail($user['email'], $user['displayName'], 'Lời mời kết bạn', $currentUser['displayName'] . " đã gửi cho bạn một lời mời kết bạn.Link đến trang cá nhân: 
     <a href = \"$BASE_URL/profile.php?id=$fromUserId\">$BASE_URL/profile.php?id=$fromUserId</a>");
-   
 }
 
-function getNewFeedsOfCurrentUser($userId) {
+function getNewFeedsOfCurrentUser($userId)
+{
     global $db;
     $friends = getFriends($userId);
     $friendIds = array();
     foreach ($friends as $friend) {
-      $friendIds[] = $friend['id'];
+        $friendIds[] = $friend['id'];
     }
     $friendIds[] = $userId;
     $stmt = $db->prepare("SELECT p.*, u.displayName , u.id as idAvatar FROM posts as p LEFT JOIN user as u ON u.id = p.userId WHERE p.userId IN (" . implode(',', $friendIds) .  ") ORDER BY createdAt DESC");
     $stmt->execute();
     $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
     return $posts;
-  }
+}
 
-  function getFriends($userId) {
+function getFriends($userId)
+{
     global $db;
     $stmt = $db->prepare("SELECT * FROM friendship WHERE userId1 = ? OR userId2 = ?");
     $stmt->execute(array($userId, $userId));
     $followings = $stmt->fetchAll(PDO::FETCH_ASSOC);
     $friends = array();
     for ($i = 0; $i < count($followings); $i++) {
-      $row1 = $followings[$i];
-      if ($userId == $row1['userId1']) {
-        $userId2 = $row1['userId2'];
-        for ($j = 0; $j < count($followings); $j++) {
-          $row2 = $followings[$j];
-          if ($userId == $row2['userId2'] && $userId2 == $row2['userId1']) {
-            $friends[] = findUserById($userId2);
-          }
+        $row1 = $followings[$i];
+        if ($userId == $row1['userId1']) {
+            $userId2 = $row1['userId2'];
+            for ($j = 0; $j < count($followings); $j++) {
+                $row2 = $followings[$j];
+                if ($userId == $row2['userId2'] && $userId2 == $row2['userId1']) {
+                    $friends[] = findUserById($userId2);
+                }
+            }
         }
-      }
     }
     return $friends;
-  }
+}
 
-  function getTotalPostOfNewfeed($userId)
-  {
+function getTotalPostOfNewfeed($userId)
+{
     global $db;
     $friends = getFriends($userId);
     $friendIds = array();
     foreach ($friends as $friend) {
-      $friendIds[] = $friend['id'];
+        $friendIds[] = $friend['id'];
     }
     $friendIds[] = $userId;
     $stmt = $db->prepare("SELECT COUNT(*) as total_post FROM posts as p LEFT JOIN user as u ON u.id = p.userId WHERE p.userId IN (" . implode(',', $friendIds) .  ") ORDER BY createdAt DESC");
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
-  }
+}
 
-  function getNewFeedsOfCurrentUserWithPagging($userId,$limit,$page) {
+function getNewFeedsOfCurrentUserWithPagging($userId, $limit, $page)
+{
     global $db;
     $friends = getFriends($userId);
     $friendIds = array();
     foreach ($friends as $friend) {
-      $friendIds[] = $friend['id'];
+        $friendIds[] = $friend['id'];
     }
     $friendIds[] = $userId;
-    $calc_page=intval(($page-1)*$limit);
-    
+    $calc_page = intval(($page - 1) * $limit);
+
     $stmt = $db->prepare("SELECT p.*, u.displayName , u.id as idAvatar FROM posts as p LEFT JOIN user as u ON u.id = p.userId WHERE p.userId IN (" . implode(',', $friendIds) .  ") ORDER BY createdAt DESC LIMIT $calc_page , $limit");
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
-  }
+}
+
+function sendMessage($fromUserID, $toUserID, $content)
+{
+    global $db;
+    $stmt = $db->prepare("INSERT INTO `message` (`id`, `fromUserID`, `toUserID`, `content`, `deleteUserID`, `createdAt`) VALUES (NULL, ?, ?, ?, NULL, CURRENT_TIMESTAMP);");
+    $stmt->execute(array($fromUserID, $toUserID, $content));
+    sendNotificationSendMessage($fromUserID, $toUserID);
+}
+
+function loadMessage($fromUserID, $toUserID)
+{
+    global $db;
+    $stmt = $db->prepare("SELECT * FROM `message` WHERE (fromUserID = ? AND toUserID = ?) OR (toUserID = ? AND fromUserID = ?) ORDER BY createdAt ASC");
+    $stmt->execute(array($fromUserID, $toUserID, $fromUserID, $toUserID));
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function sendNotificationSendMessage($fromUserId, $toUserId)
+{
+    global $BASE_URL;
+    $user = findUserById($toUserId);
+    $currentUser = findUserById($fromUserId);
+
+    sendEmail($user['email'], $user['displayName'], 'Tin nhắn mới', $currentUser['displayName'] . " đã gửi tin nhắn mới cho bạn");
+}
+
+function deleteMessage($id)
+{
+    global $db;
+    $stmt = $db->prepare("DELETE FROM `message` WHERE id = ?");
+    $stmt->execute(array($id));
+    $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
--- messager.php
+++ messager.php
@@ -1,118 +1,53 @@
 <?php
 require_once 'init.php';
-
 if (!$currentUser) {
-    header('Location:index.php');
+    header('Location: index.php');
     exit();
 }
-include 'header.php';
-
-$toUserID = findUserByID($_SESSION['id']); ?>
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<script>
-    var from = null,
-        to = null,
-        start = 0,
-        url = 'http://localhost/deadline/Project/chat.php';
-    $(document).ready(function() {
-        from = "<?php echo $currentUser['id']; ?>";
-        to = "<?php echo $toUserID['id']; ?>";
-        load();
-        $('form').submit(function(e) {
-            $.post(url, {
-                message: $('#message').val(),
-                from: from,
-                to: to
-            })
-            $('#message').val('');
-            return false;
-        })
-    })
-
-    function load() {
-        $.get(url + '?start=' + start, function(result) {
-            if (result.items) {
-                result.items.forEach(item => {
-                    start = item.id;
-                    $('#messages').append(renderMessage(item));
-                })
-                $('#message').animate({
-                    scrollTop: $('#message')[0].scrollHeight
-                });
-            };
-            load();
-        });
-    }
-    function renderMessage(item) {
-            if (item.fromUserID == "<?php echo $currentUser['id']; ?>") {
-                let time = new Date(item.createdAt);
-                time = `${time.getHours()}:${time.getMinutes() < 10 ? '0' : ''}${time.getMinutes()}`;
-                return `<div class="msg" style="float: right">${item.message}<span>${time}</span></div><br><br>`;
-            } else {
-                let time = new Date(item.createdAt);
-                time = `${time.getHours()}:${time.getMinutes() < 10 ? '0' : ''}${time.getMinutes()}`;
-                return `<div class="row"><div class="msg">${item.message}<span>${time}</span></div>`;
-            }
-            
-    }
-</script>
-<style>
-    body {
-        margin: 0;
-        overflow: hidden;
-        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-    }
-
-    #messages {
-        height: 80vh;
-        overflow: hidden;
-        padding: 10px;
-    }
-
-    form {
-        display: flex;
-    }
-
-    input {
-        font-size: 1.2rem;
-        padding: 10px;
-        margin: 10px 5px;
-        appearance: none;
-        -webkit-appearance: none;
-        border: 1px solid #ccc;
-        border-radius: 5px;
-    }
-
-    #message {
-        flex: 2
-    }
-
-    .msg {
-        background-color: #dcf8c6;
-        padding: 5px 10px;
-        border-radius: 5px;
-        margin-bottom: 8px;
-        width: fit-content
-    }
-
-    .msg p {
-        margin: 0;
-        font-weight: bold
-    }
-
-    .msg span {
-        font-size: 0.7rem;
-        margin-left: 15px
-    }
-</style>
-</head>
-
-<body>
-    <div id="messages"></div>
-    <form>
-        <input type="text" id="message" autocomplete="off" autofocus placeholder="Type message...">
-        <input type="submit" value="Send">
-    </form>
-</body>
-
-</html>
+?>
+<?php include 'header.php'; ?>
+<?php $toUserID = findUserByID($_SESSION['id']); ?>
+<?php
+$count = 0;
+$messagers = loadMessage($currentUser['id'], $toUserID['id']);
+if(isset($_POST['delete'])){
+    $id = $_POST['delete'];
+    deleteMessage($id);
+    header('Location: messager.php');
+}
+foreach ($messagers as $messager) { ?>
+    <?php if ($currentUser['id'] == $messager['fromUserID']) { ?>
+        <form class='textcolor' action="messager.php" method="POST" enctype="multipart/form-data">
+            <div class="card" style="text-align: right">
+                <div class="card-body">
+                    <p class="card-text"> <?php echo $messager['content']; ?> <span style="font-size: 10px"><?php echo $messager['createdAt']; ?></span> </p>
+                </div>
+                <button type="submit" value="<?php echo $messager['id']; ?>" name="delete" class="btn btn-outline-danger">Delete</button>
+            </div>
+        </form>
+    <?php } ?>
+    <?php if ($currentUser['id'] != $messager['fromUserID']) { ?>
+        <div class="card">
+            <div class="card-body">
+                <p class="card-text"> <?php echo $messager['content']; ?> <span style="font-size: 10px"><?php echo $messager['createdAt']; ?></span></p>
+            </div>
+        </div>
+    <?php } ?>
+<?php } ?>
+<?php if (isset($_POST['send']) && isset($_POST['message'])) {
+    $message = $_POST['message'];
+    sendMessage($currentUser['id'], $toUserID['id'], $message);
+    header('Location: messager.php');
+}
+?>
+<form class='textcolor' action="messager.php" method="POST" enctype="multipart/form-data">
+    <div class="form-group">
+        <div class="row" style="margin-top: 50%">
+            <div class="col-sm-10" style="margin-top: 1%">
+                <input type="text" name="message" class="form-control form-control-lg" autocomplete="off" autofocus placeholder="Viết tin nhắn vào đây...">
+            </div>
+            <button type="submit" name="send" class="btn btn-outline-primary btn-lg">Gửi tin nhắn</button>
+        </div>
+    </div>
+</form>
+<?php include 'footer.php'; ?>
